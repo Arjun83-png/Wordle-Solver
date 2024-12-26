db1c9290a32712d2647e8fe53ee9e3d49a3d88f9
Arjun83-png <70520684+Arjun83-png@users.noreply.github.com>
2024-12-25 19:54:43 -0800

    Update demo_functions.c

--- demo_functions.c
+++ demo_functions.c
@@ -1,4 +1,4 @@
-// A nice place for you to mess with the functions, while you're developing.
+
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -12,18 +12,16 @@ int main(void) {
   char words[10][6] = {"stalk", "scrap", "shear", "batch", "motif",
                        "tense", "ultra", "vital", "ether", "nadir"};
 
-  // make a dynamically-allocated vocabulary that you can mess with, including
-  // freeing elements of it when necessary.
+
   char **vocabulary = calloc(10, sizeof(char *));
   for (int i = 0; i < 10; i++) {
     vocabulary[i] = strdup(words[i]);
   }
   size_t num_words = 10;
 
-  // TODO(you): exercise all of your functions in here. Write some good test
-  // cases!
 
-  // ... OK we're done, clean up the vocabulary.
+
+
   free_vocabulary(vocabulary, num_words);
 
   return 0;
